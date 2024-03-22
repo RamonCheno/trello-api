d877a547475b4776058fbae045b0a7402706aec9
RamonCheno <francisco.cheno.ocano@gmail.com>
2024-03-21 22:09:19 -0700

    update docker

--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
 const express = require("express");
 const routerApi = require('./routes/index.router');
-const swaggerDocs = require( "./controller/swagger.controller");
+const swaggerDocs = require("./controller/swagger.controller");
 const cors = require("cors");
 const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
-const corsOpton = {
-    origin: "http://192.168.1.82:5173",
-    methods: ["POST", "GET"],
-    credential: true,
-}
+// const corsOpton = {
+//     origin: "http://192.168.1.82:5173",
+//     methods: ["POST", "GET"],
+//     credential: true,
+// }
+
+const corsOption = {
+    origin: "http://localhost:3000"
+};
 
 //middlewares
+app.use(cors(corsOption));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors(corsOpton));
 
 //routes
+
 routerApi(app);
+
+app.use(swaggerDocs);
 
 app.listen(port, () => {
     console.log("servidor en puerto ", port);
 });
-app.use(swaggerDocs);
