9c02e821a2203f5a46d7d8f700b6b99fcac931b3
RamonCheno <francisco.cheno.ocano@gmail.com>
2024-04-05 22:33:27 -0700

    prepara para merch a rama master

--- src/config/database.js
+++ src/config/database.js
@@ -2,18 +2,27 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    // "username": process.env.POSTGRES_USER,
-    // "password": process.env.POSTGRES_PASSWORD,
-    // "database": process.env.POSTGRES_DBNAME,
-    // "host": process.env.POSTGRES_HOST,
-    // "dialect": 'postgres',
-    // 'port': process.env.POSTGRES_PORT
-    "url": process.env.DATABASE_URL
+    "username": process.env.DATABASE_USERDEV,
+    "password": process.env.DATABASE_PASSWORDDEV,
+    "database": process.env.DATABASE_NAMEDEV,
+    "host": process.env.DATABASE_HOSTDEV,
+    "dialect": 'postgres',
+    'port': process.env.DATABASE_PORTDEV
   },
   "test": {
-    "url": process.env.DATABASE_URL
+    "username": process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DBNAME,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": 'postgres',
+    // 'port': process.env.POSTGRES_PORT
   },
   "production": {
-    "url": process.env.DATABASE_URL
+    "username": process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DBNAME,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": 'postgres',
+    // 'port': process.env.POSTGRES_PORT
   }
 }
