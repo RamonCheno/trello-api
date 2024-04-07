7ebc413f31c0ebf94dd0a79b32468087cda4ae82
RamonCheno <francisco.cheno.ocano@gmail.com>
2024-04-06 21:38:19 -0700

    update database

--- src/config/database.js
+++ src/config/database.js
@@ -3,32 +3,34 @@
 // });
 
 module.exports = {
-  "development": {
-    "username": process.env.DATABASE_USER,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE_NAME,
-    "host": process.env.DATABASE_HOST,
-    "dialect": 'postgres',
-    "dialectOptions": {
-      "ssl": 'require'
+  development: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: 'require'
     }
   },
   "test": {
-    "username": process.env.DATABASE_USER,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE_NAME,
-    "host": process.env.DATABASE_HOST,
-    "dialect": 'postgres',
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres',
     // 'port': process.env.POSTGRES_PORT
   },
-  "production": {
-    "username": process.env.DATABASE_USER,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE_NAME,
-    "host": process.env.DATABASE_HOST,
-    "dialect": 'postgres',
-    "dialectOptions": {
-      "ssl": 'require'
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true
+      }
     }
     // 'port': process.env.POSTGRES_PORT
   }
